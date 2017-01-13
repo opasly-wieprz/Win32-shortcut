c7415cf1ea260390317a22cbf0a695d85c5ba00a
Piotr Latanowicz <piotr.latanowicz@gmail.com>
2017-01-13 15:13:07 +0100

    mainly docs

--- src/System/Win32/Shortcut.hs
+++ src/System/Win32/Shortcut.hs
@@ -1,34 +1,78 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RankNTypes #-}
 
+-- |
+-- Working with @.lnk@ format should be a matter of serializing.
+-- This library takes simpler approach, utilizing @Component Object Model@
+-- (@COM@) library. Even though @COM@ provides some means of serialization,
+-- they cannot be used in a pure fashion - the library needs
+-- to be initialized and some @COM@ functions still query the
+-- system for data. For this reason this library sticks to 'IO'.
+--
+-- Before calling 'writeShortcut' or 'readShortcut', @COM@ library
+-- must be initialized with 'initialize'.
+--
+-- Library does not support shortcut's @IDList@s, so creating or
+-- reading links to devices or network connections is not possible.
+--
+-- === Example
+-- @
+-- import Control.Monad.Except
+--
+-- main = print . runExceptT $ do
+--   let link = empty { targetPath = "notepad.exe" }
+--
+--   ExceptT initialize
+--   ExceptT $ writeShortcut link "c:\\\\link.lnk"
+--   ret <- ExceptT $ readShortcut "c:\\\\link.lnk"
+--   liftIO $ uninitialize
+--
+--   return ret
+-- @
+-- @
+-- >>> main
+-- Right (Shortcut {targetPath = "C:\\\\Windows\\\\system32\\\\notepad.exe",
+-- arguments = "", workingDirectory = "", showCmd = ShowNormal,
+-- description = "", iconLocation = ("",0), hotkey = 0})
+-- @
 module System.Win32.Shortcut (
+
   Shortcut (..),
+  empty,
   ShowCmd (..),
+
+  -- * Basic operations
   writeShortcut,
   unsafeWriteShortcut,
   readShortcut,
+
+  -- * COM initialization
   initialize,
   uninitialize,
-  empty,
 
-  WORD,
-  HRESULT,
-  HRESULTError (..),
-  OrHRESULTError (..),
-  CoCreateInstanceError (..),
-  CoInitializeError (..),
-  SaveError (..),
+  -- * Errors
+  ShortcutError (..),
+
+  -- ** File IO Errors
   LoadError (..),
+  SaveError (..),
+
+  -- ** Argument errors
   PathError (..),
   ArgumentsError (..),
   WorkingDirectoryError (..),
   DescriptionError (..),
   IconLocationError (..),
-  Error (..)
+
+  -- ** Other errors
+  CoCreateInstanceError (..),
+  CoInitializeError (..),
+  HRESULTError (..),
+  OrHRESULTError (..)
 )
 where
 
-import Control.Monad (join, when, void)
+import Control.Monad (when, void)
 import Control.Monad.Cont (ContT (..))
 import Control.Monad.Except (ExceptT (..), withExceptT, runExceptT, throwError)
 import Control.Monad.IO.Class (liftIO)
@@ -39,10 +83,15 @@ import Foreign.C (peekCWString)
 import System.Win32.Shortcut.Error
 import System.Win32.Shortcut.Internal
 
+
+-- | Defines how a window will be opened when a link is executed.
 data ShowCmd
   = ShowNormal
+  -- ^ Start normally.
   | ShowMaximized
+  -- ^ Start maximized.
   | ShowMinimized
+  -- ^ Start minimized.
   deriving (Show)
 
 fromShowCmd :: ShowCmd -> CInt
@@ -59,14 +108,34 @@ toShowCmd x
   | otherwise               = ShowNormal
 
 
+-- | A shell link.
+--
+--   It seems that @.lnk@ format permits up to 32767
+--   characters in text fields (259 for 'targetPath'), however
+--   if 'workingDirectory', 'description' or 'iconLocation' is
+--   longer then 259 characters @COM@ won't be able to read the
+--   shortcut corectly ('readShortcut' will return faulty link without
+--   raising any error, and @explorer.exe@ may not interpret it properly).
+--   For this reason two write functions are provided. 'unsafeWriteShortcut'
+--   will allow long fields and 'writeShortcut' which will raise
+--   error if 'workingDirectory', 'description' or 'iconLocation'
+--   is longer then 259 characters.
 data Shortcut = Shortcut {
-  targetPath       :: FilePath,
-  arguments        :: String,
+  targetPath :: FilePath,
+  -- ^ Path to target.
+  arguments :: String,
+  -- ^ Arguments for target.
   workingDirectory :: FilePath,
-  showCmd          :: ShowCmd,
-  description      :: String,
-  iconLocation     :: (FilePath, Int),
-  hotkey           :: WORD
+  -- ^ Path to working directory.
+  showCmd :: ShowCmd,
+  description :: String,
+  iconLocation :: (FilePath, Int),
+  -- ^ Path to icon container (e.g. @.exe@, @.dll@, or @.ico@ file)
+  --   and icon index.
+  hotkey :: WORD
+  -- ^ The virtual key code is in the low-order byte,
+  --   and the modifier flags are in the high-order byte.
+  --   @'hotkey' == 0@ means no hotkey will be used.
 } deriving (Show)
 
 
@@ -188,56 +257,53 @@ withIPersistFileCaller shellLinkCaller =
     ipRelease'
 
 
-withExcept'
-  :: (HRESULTError -> Either e ())
-  -> ContT r IO (IO HRESULT)
-  -> ExceptT e (ContT r IO) ()
-withExcept' f = ExceptT . fmap (succeeded' f) . join . fmap lift
-
-overLeft :: (e1 -> e2) -> Either e1 a -> Either e2 a
-overLeft f = either (Left . f) Right
-
-
--- including terminator
+-- Max length of a CString fields, including terminator
 longFieldLength, shortFieldLength :: CInt
 longFieldLength = 32768
-shortFieldLength = mAX_PATH + 1
+shortFieldLength = mAX_PATH + 1 -- does not apply to targetPath
 
 
-writeShortcut :: Shortcut -> FilePath -> IO (Either Error ())
-writeShortcut = writeShortcutGeneral True
+-- | Create a shortcut under specified location. 'initialize' must be
+--   called beforehand. 'targetPath' will be resolved with
+--   respect to whatever is found in @PATH@ variable or desktop
+--   if saved path is not absolute.
+writeShortcut:: Shortcut -> FilePath -> IO (Either ShortcutError ())
+writeShortcut = writeShortcutGeneric True
 
-unsafeWriteShortcut :: Shortcut -> FilePath -> IO (Either Error ())
-unsafeWriteShortcut = writeShortcutGeneral False
+-- | Same as 'writeShortcut', but allows long 'description',
+--   'workingDirectory' and 'iconLocation' fields. @COM@ and @explorer.exe@
+--   may not interpret created link correctly.
+unsafeWriteShortcut :: Shortcut -> FilePath -> IO (Either ShortcutError ())
+unsafeWriteShortcut = writeShortcutGeneric False
 
-writeShortcutGeneral :: Bool -> Shortcut -> FilePath -> IO (Either Error ())
-writeShortcutGeneral safeRead shortcut path = flip runContT return . runExceptT $ do
+writeShortcutGeneric :: Bool -> Shortcut -> FilePath -> IO (Either ShortcutError ())
+writeShortcutGeneric safeRead shortcut path = flip runContT return . runExceptT $ do
 
   let throwIfTooLong f maxLength err = when (length (f shortcut) >= fromIntegral maxLength) (throwError err)
       throwIfTooLong' f = throwIfTooLong f (if safeRead then shortFieldLength else longFieldLength) in
-    do throwIfTooLong targetPath mAX_PATH        (InvalidPath $      OtherError PathTooLong)
+    do throwIfTooLong targetPath mAX_PATH        (InvalidPath      $ OtherError PathTooLong)
        throwIfTooLong arguments  longFieldLength (InvalidArguments $ OtherError ArgumentsTooLong)
 
        throwIfTooLong' workingDirectory     (InvalidWorkingDirectory $ OtherError WorkingDirectoryTooLong)
-       throwIfTooLong' description          (InvalidDescription $      OtherError DescriptionTooLong)
-       throwIfTooLong' (fst . iconLocation) (InvalidIconLocation $     OtherError IconLocationTooLong)
+       throwIfTooLong' description          (InvalidDescription      $ OtherError DescriptionTooLong)
+       throwIfTooLong' (fst . iconLocation) (InvalidIconLocation     $ OtherError IconLocationTooLong)
 
   shellLinkCaller <- withExceptT CreateIShellLinkInterfaceError withIShellLinkCaller
 
   withExcept' (Left . InvalidPath . HRESULTError) $
-    call shellLinkCaller setPath' <$> ContT (withCWString (targetPath shortcut))
+    call shellLinkCaller setPath' <$> ContT (withCWString $ targetPath shortcut)
 
   withExcept' (Left . InvalidArguments . HRESULTError) $
-    call shellLinkCaller setArguments' <$> ContT (withCWString (arguments shortcut))
+    call shellLinkCaller setArguments' <$> ContT (withCWString $ arguments shortcut)
 
   withExcept' (Left . InvalidWorkingDirectory . HRESULTError) $
-    call shellLinkCaller setWorkingDirectory' <$> ContT (withCWString (workingDirectory shortcut))
+    call shellLinkCaller setWorkingDirectory' <$> ContT (withCWString $ workingDirectory shortcut)
 
   withExcept' (Left . InvalidShowCmd) $
     call shellLinkCaller setShowCmd' <$> pure (fromShowCmd $ showCmd shortcut)
 
   withExcept' (Left . InvalidDescription . HRESULTError) $
-    call shellLinkCaller setDescription' <$> ContT (withCWString (description shortcut))
+    call shellLinkCaller setDescription' <$> ContT (withCWString $ description shortcut)
 
   let (iconLocation', iconIndex) = iconLocation shortcut
   withExcept' (Left . InvalidIconLocation . HRESULTError) $
@@ -253,7 +319,9 @@ writeShortcutGeneral safeRead shortcut path = flip runContT return . runExceptT 
     call iPersistFileCaller save' <$> ContT (withCWString path) <*> pure tRUE
 
 
-readShortcut :: FilePath -> IO (Either Error Shortcut)
+-- | Read a shortcut from the supplied location. 'initialize' must be
+--   called beforehand.
+readShortcut :: FilePath -> IO (Either ShortcutError Shortcut)
 readShortcut path = flip runContT return . runExceptT $ do
 
   shellLinkCaller <- withExceptT CreateIShellLinkInterfaceError withIShellLinkCaller
@@ -264,7 +332,7 @@ readShortcut path = flip runContT return . runExceptT $ do
   withExcept' (overLeft LoadError . toLoadError) $
     call iPersistFileCaller load' <$> ContT (withCWString path) <*> pure sTGM_READ
 
-  pathPtr <- lift . ContT $ allocaArray (fromIntegral $ mAX_PATH)
+  pathPtr <- lift . ContT $ allocaArray (fromIntegral mAX_PATH)
   withExcept' (overLeft InvalidPath . toPathError) . pure $
     call shellLinkCaller getPath' pathPtr mAX_PATH nullPtr sLGP_RAWPATH
 
@@ -303,12 +371,20 @@ readShortcut path = flip runContT return . runExceptT $ do
                       <*> (fromIntegral <$> peek iconIndexPtr))
              <*> peek hotkeyPtr
 
-initialize :: IO (Either (OrHRESULTError CoInitializeError) ())
-initialize = succeeded' toCoInitializeError <$> c_CoInitialize nullPtr
 
+-- | Initialize @COM@ library for current thread.
+--   Wraps <https://msdn.microsoft.com/en-us/library/windows/desktop/ms695279(v=vs.85).aspx CoInitializeEx>
+--   function.
+initialize :: IO (Either ShortcutError ())
+initialize = succeeded' (overLeft InitializationError . toCoInitializeError)
+  <$> c_CoInitializeEx nullPtr cOINIT_MULTITHREADED
+
+-- | Uninitialize @COM@ library for current thread.
 uninitialize :: IO ()
 uninitialize = c_CoUninitialize
 
+
+-- | An empty link. All fields are set to empty/default values.
 empty :: Shortcut
 empty = Shortcut {
   targetPath = "",
--- src/System/Win32/Shortcut/Error.hs
+++ src/System/Win32/Shortcut/Error.hs
@@ -3,30 +3,37 @@
 module System.Win32.Shortcut.Error
 where
 
+import Control.Monad (join)
+import Control.Monad.Except (ExceptT (..))
+import Control.Monad.Trans (MonadTrans, lift)
+
 import System.Win32.Shortcut.Internal
 
+-- | In @Win32 API@ 'HRESULT' is used to indicate error and warning
+--   conditions. 'HRESULTError' wraps 'HRESULT' to give it some meaning.
 data HRESULTError
   = E_ABORT
-  -- ^ Operation aborted
+  -- ^ Operation aborted.
   | E_ACCESSDENIED
-  -- ^ General access denied error
+  -- ^ General access denied error.
   | E_FAIL
-  -- ^ Unspecified failure
+  -- ^ Unspecified failure.
   | E_HANDLE
-  -- ^ Handle that is not valid
+  -- ^ Handle that is not valid.
   | E_INVALIDARG
-  -- ^ One or more arguments are not valid
+  -- ^ One or more arguments are not valid.
   | E_NOINTERFACE
-  -- ^ No such interface supported
+  -- ^ No such interface supported.
   | E_NOTIMPL
-  -- ^ Not implemented
+  -- ^ Not implemented.
   | E_OUTOFMEMORY
-  -- ^ Failed to allocate necessary memory
+  -- ^ Failed to allocate necessary memory.
   | E_POINTER
-  -- ^ Pointer that is not valid
+  -- ^ Pointer that is not valid.
   | E_UNEXPECTED
-  -- ^ Unexpected failure
+  -- ^ Unexpected failure.
   | HRESULTUnknown HRESULT
+  -- ^ Other, unknown 'HRESULT' value.
   deriving (Show)
 
 succeeded :: HRESULT -> Either HRESULTError ()
@@ -44,15 +51,28 @@ succeeded x
   | otherwise           = Left (HRESULTUnknown x)
 
 succeeded' :: (HRESULTError -> Either e ()) -> HRESULT -> Either e ()
-succeeded' f = either f (Right . id) . succeeded
+succeeded' f = either f Right . succeeded
+
+withExcept'
+  :: (MonadTrans t, Monad (t m), Monad m)
+  => (HRESULTError -> Either e ())
+  -> t m (m HRESULT)
+  -> ExceptT e (t m) ()
+withExcept' f = ExceptT . fmap (succeeded' f) . join . fmap lift
+
+overLeft :: (e1 -> e2) -> Either e1 a -> Either e2 a
+overLeft f = either (Left . f) Right
 
 
-data OrHRESULTError a
-  = OtherError a
+-- | Some error that comes from 'HRESULT'.
+data OrHRESULTError err
+  = OtherError err
+  -- ^ 'HRESULT' value was expected and fits @err@.
   | HRESULTError HRESULTError
+  -- ^ 'HRESULT' did not fit @err@.
   deriving (Show)
 
-
+-- | @COM@ cannot create specified interface.
 data CoCreateInstanceError
   = CCI_REGDB_E_CLASSNOTREG
   -- ^ A specified class is not registered in the registration database.
@@ -66,33 +86,48 @@ data CoCreateInstanceError
   --   or the controlling IUnknown does not expose the requested interface.
   | CCI_E_POINTER
   -- ^ The ppv parameter is NULL.
+  | CCI_CO_E_NOTINITIALIZED
+  -- ^ @COM@ library was not initialized.
   deriving (Show)
 
 toCoCreateInstanceError :: HRESULTError -> Either (OrHRESULTError CoCreateInstanceError) ()
 toCoCreateInstanceError = Left . \case
   HRESULTUnknown x
-    | x == rEGDB_E_CLASSNOTREG   -> OtherError CCI_REGDB_E_CLASSNOTREG
-    | x == cLASS_E_NOAGGREGATION -> OtherError CCI_CLASS_E_NOAGGREGATION
+    | x == -2147221164 -- 0x80040154
+      -> OtherError CCI_REGDB_E_CLASSNOTREG
+    | x == -2147221232 -- 0x80040110
+      -> OtherError CCI_CLASS_E_NOAGGREGATION
+    | x == -2147221008 -- 0x800401f0
+      -> OtherError CCI_CO_E_NOTINITIALIZED
   E_NOINTERFACE -> OtherError CCI_E_NOINTERFACE
   E_POINTER     -> OtherError CCI_E_POINTER
   other         -> HRESULTError other
 
 
+-- | @COM@ failed to initialize.
 data CoInitializeError
   = CI_RPC_E_CHANGED_MODE
   -- ^ A previous call to CoInitializeEx specified the concurrency model
   --   for this thread as multithread apartment (MTA).
   --   This could also indicate that a change from neutral-threaded
   --   apartment to single-threaded apartment has occurred.
+  | CI_CO_E_NOTINITIALIZED
+  -- ^ You need to initialize the @COM@ library on a thread
+  --   before you call any of the library functions.
+  --   Otherwise, the @COM@ function will return 'CI_CO_E_NOTINITIALIZED'.
   deriving (Show)
 
 toCoInitializeError :: HRESULTError -> Either (OrHRESULTError CoInitializeError) ()
 toCoInitializeError = Left . \case
   HRESULTUnknown x
-    | x == rPC_E_CHANGED_MODE -> OtherError CI_RPC_E_CHANGED_MODE
+    | x == -2147417850 -- 0x80010106
+      -> OtherError CI_RPC_E_CHANGED_MODE
+    | x == -2147221008 -- 0x800401F0
+      -> OtherError CI_CO_E_NOTINITIALIZED
   other -> HRESULTError other
 
 
+-- | @IPersistFile@ interface failed to save a file.
 data SaveError
   = SE_S_FALSE
   -- ^ The object was not successfully saved.
@@ -105,6 +140,7 @@ toSaveError = \case
   other -> Left (HRESULTError other)
 
 
+-- | @IPersistFile@ interface failed to load a file.
 data LoadError
   = LE_E_OUTOFMEMORY
   -- ^ The object could not be loaded due to a lack of memory.
@@ -124,7 +160,7 @@ data PathError
   = Path_S_FALSE
   -- ^ The operation is successful but no path is retrieved.
   | PathTooLong
-  -- ^ Path length >= MAX_PATH characters.
+  -- ^ Path length >= 260 characters.
   deriving (Show)
 
 toPathError :: HRESULTError -> Either (OrHRESULTError PathError) ()
@@ -142,24 +178,23 @@ data ArgumentsError
 
 data WorkingDirectoryError
   = WorkingDirectoryTooLong
-  -- ^ Working directory length >= 32768 characters.
   deriving (Show)
 
 
 data DescriptionError
   = DescriptionTooLong
-  -- ^ Description length >= 32768 characters.
   deriving (Show)
 
 
 data IconLocationError
   = IconLocationTooLong
-  -- ^ Icon location path length >= 32768 characters.
   deriving (Show)
 
 
-data Error
-  = CreateIShellLinkInterfaceError   (OrHRESULTError CoCreateInstanceError)
+-- | Catch-all type for errors.
+data ShortcutError
+  = InitializationError              (OrHRESULTError CoInitializeError)
+  | CreateIShellLinkInterfaceError   (OrHRESULTError CoCreateInstanceError)
   | CreateIPersistFileInterfaceError (OrHRESULTError CoCreateInstanceError)
   | LoadError                        (OrHRESULTError LoadError)
   | SaveError                        (OrHRESULTError SaveError)
